bf7d04be5a4ba3f69691dd1dcf7cc56e21731cfa
Colleen Garvey <colleenellengarvey@gmail.com>
2022-07-26 13:39:27 -0400

    all exercises complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum('annual_revenue')
+
+average_revenue = total_revenue / Store.count
+
+million_revenue_stores = Store.where('annual_revenue >= 1000000')
+
+puts "There are #{million_revenue_stores.count} stores that make 1+ million revenue annually"
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@store2.employees.create(first_name: "Colleen", last_name: "Garvey", hourly_rate: 100)
+employee = Employee.create(first_name: "Tom", last_name: "Smith", hourly_rate: 55)
+
+store = Store.create(name: "Store One", annual_revenue: 550000, womens_apparel: true)
+store = Store.create(name: "Store Two", mens_apparel: true, womens_apparel: true)
+store = Store.create(name: "Store Three", annual_revenue: 0, mens_apparel: true, womens_apparel: true)
+puts "Store name:"
+store_name = gets.chomp
+puts "Creating store #{store_name}..."
+
+invalid_store = Store.create(name: store_name)
+
+valid_store_check = invalid_store.valid?
+puts "Is store valid?: #{valid_store_check}"
+
+puts invalid_store.errors.full_messages
